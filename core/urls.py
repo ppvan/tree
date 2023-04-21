4d7c9580af05a9516530d8bb5edb64beb888bc65
hoanganhnth <hoanganhnth2k3@gmail.com>
2023-04-21 10:41:01 +0700

    list, update,delete,add  product

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
-from .views import AddItemView, delete_item, list_item, update_item
+from .views import (
+    AddItemView, delete_item, list_item, update_item,list_product,delete_product,list_catagory,list_post,add_product,update_product)
 
 app_name = 'core'
 urlpatterns = [
-    path('add-product/', AddItemView.as_view(), name='add_item'),
-    path('list-product/', list_item, name='list_item'),
-    path('delete-product/<int:pk>', delete_item, name='delete_item'),
-    path('update-product/<int:pk>', update_item, name='update_item'),
+    path('list-product/',list_product,name='list_product'),
+    path('delete-product/<int:pk>', delete_product, name='delete_product'),
+    path('list-catagory/',list_catagory,name='list_catagory'),
+    path('list-post/',list_post,name='list_post'),
+    path('add-product/', add_product, name='add_product'),
+    path('update-product/<int:pk>', update_product, name='update_product'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import HttpResponse, redirect, render
 from django.views import View
 
-from .forms import ProductForm
-from .models import Product
+from .forms import ProductForm,ProductImageForm
+from .models import Product,ProductImage
 
 
 # Create your views here.
@@ -51,3 +51,61 @@ def update_item(request, pk):
             return redirect('/core/list-item/')
     context = {'form': form}
     return render(request, 'update_item.html', context)
+
+
+# admin
+def list_product(request):
+    p = Product.objects.all()
+    context = {'products': p}
+    return render(request,'administrator/list_product.html',context)
+
+def delete_product(request, pk):
+    p = Product.objects.get(id=pk)
+    if request.method == "POST":
+        p.delete()
+        return redirect('/core/list-product/')
+    return render(request, 'administrator/delete_product.html', {'pr': p})
+
+
+def list_catagory(request):
+    return render(request,'administrator/list_catagory.html')
+
+
+def list_post(request):
+    return render(request,'administrator/list_post.html')
+
+
+def add_product(request):
+    p = ProductForm()
+    i = ProductImageForm()
+    if request.method == "POST":
+        i = ProductImageForm(request.POST, request.FILES)
+        files = request.FILES.getlist("image")
+        p = ProductForm(request.POST, request.FILES)
+        if not p.is_valid():
+            return HttpResponse("ban nhap sai du lieu")
+        product=p.save()
+        for file in files: 
+            ProductImage.objects.create(product=product, image=file)
+
+        return HttpResponse("da them san pham")
+    
+    context = {'pro_form': p, 'img_form': i}
+    return render(request, "administrator/add_product.html", context)
+    
+
+def update_product(request, pk):
+    product = Product.objects.get(id=pk)
+    img_form = ProductImageForm()
+    pro_form = ProductForm(instance=product)
+    if request.method == 'POST':
+        pro_form = ProductForm(request.POST,request.FILES, instance=product)
+        img_form = ProductImageForm(request.POST, request.FILES)
+        files = request.FILES.getlist("image")
+        if pro_form.is_valid():
+            product=pro_form.save()
+            for file in files: 
+                ProductImage.objects.create(product=product, image=file)
+            return redirect('/core/list-product/')
+    context = {'pro_form': pro_form, 'img_form': img_form}
+    return render(request, 'administrator/update_product.html', context)
