4bb1cec345abfd2fc5a2fa41c0fcc72c105d6b8f
Phạm Văn Phúc <phuclaplace@gmail.com>
2023-05-04 09:23:57 +0700

    Secure applications

--- tree/settings.py
+++ tree/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(*z(ls(3j6qlj0zrepr0js3t7uly-#adzqj%hk(+6i#(zbi&)#"
-
+SECRET_KEY = os.getenv("SECRET_KEY", "os")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -158,17 +158,14 @@ LOGIN_URL = "login"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
 
-"""In production, you should set the following environment variables:
-But this is just my second gmail account, so I don't care if it's exposed.
-"""
 
 if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
     # SMTP settings
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST_USER = "phucdevmail@gmail.com"
-    EMAIL_HOST_PASSWORD = "lcfxnceqhdvwqkla"
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
