f10460c28321cbe5e829f15e95999579c4cf8f65
Phạm Văn Phúc <phuclaplace@gmail.com>
2023-05-10 06:42:24 +0700

    pagination  error fix

--- core/views.py
+++ core/views.py
@@ -72,7 +72,27 @@ class ProductListView(AdminRequiredMixin, ListView):
     template_name = "core/products_list.html"
     context_object_name = "products"
     ordering = ["-updated_at"]
-    paginate_by = 50
+    paginate_by = 3
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        if not context.get("is_paginated", False):
+            return context
+
+        paginator = context.get("paginator")
+        num_pages = paginator.num_pages
+        current_page = context.get("page_obj")
+        page_no = current_page.number
+
+        if num_pages <= 11 or page_no <= 6:  # case 1 and 2
+            pages = [x for x in range(1, min(num_pages + 1, 12))]
+        elif page_no > num_pages - 6:  # case 4
+            pages = [x for x in range(num_pages - 10, num_pages + 1)]
+        else:  # case 3
+            pages = [x for x in range(page_no - 5, page_no + 6)]
+
+        context.update({"pages": pages})
+        return context
 
 
 class DetailProductView(DetailView):
