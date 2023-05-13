33bf774c2e2f7dc7bc257885a171956974f39be1
Phạm Văn Phúc <phuclaplace@gmail.com>
2023-05-13 16:32:57 +0700

    Small fix

--- core/views.py
+++ core/views.py
@@ -311,10 +311,9 @@ class OrderListView(LoginRequiredMixin, ListView):
     template_name = "core/order_list.html"
     context_object_name = "orders"
     paginate_by = 3
-    ordering = ["-updated_at"]
 
     def get_queryset(self):
-        return Order.objects.filter(user=self.request.user)
+        return Order.objects.filter(user=self.request.user).order_by("-updated_at")
 
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
